33679dc09d4a356b9323f9d8ce1927e2cfe54515
John Clark <wildnotion@gmail.com>
2024-11-10 15:08:42 -0800

    Module 2 Completed Baseline

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const ctrlMain = require('../controllers/main'); //Added in Module Two
 
 /* GET home page. */
+router.get('/', ctrlMain.index); //Added in Module Two
+
+/* Removed during Module Two
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
 
 module.exports = router;
