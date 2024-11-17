c8a1f30bca8c6ac4333e9ba8d866e74396f3fc02
John Clark <wildnotion@gmail.com>
2024-11-17 13:33:10 -0800

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,11 @@
+// Added in Module Three
+var fs = require('fs'); // Required for the filesystem to read JSON files 
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 // Added in Module Two
 /* GET travel view */ 
 const travel = (req, res) => {
-    res.render('travel', { title: "Travlr Getaways"});
+    res.render('travel', { title: "Travlr Getaways", trips}); // Mod3 "trips" added for handlebars to render
 };
 
 module.exports = {
