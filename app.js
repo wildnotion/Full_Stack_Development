5786bf9c1c3a434341b888f5ad16a9aa31f75506
wildnotion <wildnotion@gmail.com>
2024-12-15 21:55:03 -0500

    Module 6 Completed Baseline

--- app.js
+++ app.js
@@ -30,6 +30,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Added in Module Six
+// Allow CORS
+app.use("/api", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter); // Added in Module Two
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,8 +4,16 @@ const router = express.Router();
 
 const tripsController = require("../controllers/trips");
 
-router.route("/trips").get(tripsController.tripsList);
+router
+  .route("/trips")
+  .get(tripsController.tripsList)
+  //.post(authenticateJWT, tripsController.tripsAddTrip);
+  .post(tripsController.tripsAddTrip);
 
-router.route("/trips/:tripCode").get(tripsController.tripsFindByCode); // Added in Module Five
+router
+  .route("/trips/:tripCode")
+  .get(tripsController.tripsFindByCode)
+  //.put(authenticateJWT, tripsController.tripsUpdateTrip);
+  .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
